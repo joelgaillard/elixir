cbfcbdde939971022a8a4554afcc7d127c039f1f
AndreiaFerreiraOli <andreia.ferreiraoliveira@heig-vd.ch>
2024-12-09 16:08:25 +0100

    creation des composants chat et createButton

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,11 +2,16 @@
     <div>
       <h1>Chat</h1>
       <p>Bienvenue sur la page d'accueil !</p>
+      <ChatMessage />
     </div>
   </template>
   
   <script>
+  import ChatMessage from '../components/ChatMessage.vue';
   export default {
     name: 'Chat',
+    components: {
+      ChatMessage
+    }
   };
   </script>
